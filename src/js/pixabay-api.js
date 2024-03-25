c686d9062d55531332dad6c73a847a9e8eccf73e
MaxsimJSDeveloper <147420858+MaxsimJSDeveloper@users.noreply.github.com>
2024-03-25 22:02:06 +0200

    logic

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_KEY = '42920910-4418ce5fcb505a601cb14954f';
+const baseURL = 'https://pixabay.com/api/';
+
+export async function fetchImages(searchQuery, page, perPage) {
+  try {
+    const response = await axios.get(baseURL, {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: perPage,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
